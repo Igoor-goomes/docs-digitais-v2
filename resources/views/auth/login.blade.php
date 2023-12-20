ed12730ce5feecfbfd9bf5f984a0be0e5c4a8872
Igor Gomes <igorgomesdebrito.dev@gmail.com>
2023-12-20 00:01:49 -0300

    [CHANGE] - Corrigindo Projeto

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,56 @@
-@extends('layouts.index-login')
-@section('titulo', 'Login')
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-@section('page')
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <div class="conteudo-pagina">
-        <div class="titulo-pagina">
-            <h1>Gestão de Estoque | Login</h1>
-        </div>
-        <div class="informacao-pagina">
-            <div style="width: 30%; margin-left: auto; margin-right: auto">
-                <form action="{{ route('login') }}" method="post">
-                    @csrf
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <input name="email" value="{{ old('email') }}" type="text" class="form-control"
-                            placeholder="E-mail">
-                        @if ($errors->has('email'))
-                            <span class="help-block">{{ $errors->first('email') }}</span>
-                        @endif
-                    </div>
-                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <input name="password" value="{{ old('password') }}" type="password" class="form-control"
-                            placeholder="Senha">
-                        @if ($errors->has('password'))
-                            <span class="help-block">{{ $errors->first('password') }}</span>
-                        @endif
-                    </div>
-                    <button type="submit" class="btn btn-success">Acessar</button>
-                </form>
-                @if ($errors->has('usuario') || $errors->has('senha'))
-                    <span class="help-block">Necessário realizar login para ter acesso a página</span>
-                @endif
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
-        </div>
 
-    </div>
-@endsection
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+            </div>
+
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
