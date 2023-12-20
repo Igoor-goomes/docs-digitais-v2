52274ecb908e0c29c89be8ac3cbbcbce2dd6a032
Igor Gomes <igorgomesdebrito.dev@gmail.com>
2023-12-20 00:13:51 -0300

    [ADD] - Criando primeiro usuário do sistema

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-class UserTableSeeder extends Seeder
+class userTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
